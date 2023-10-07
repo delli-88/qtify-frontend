37e0802a5e279e8c43997a183e0c290fe2890944
virinchi <rangerdelli@gmail.com>
2023-10-08 01:04:53 +0530

    card-qtify-takehome

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './Card.module.css'
+
+const Card = ({cardImage, followers, title}) => {
+  return (
+    <div className={styles.cardContainer}>
+        <div className={styles.cardBody}>
+            <img src={cardImage} alt="cardImage" className={styles.cardImage}/>
+            <p className={styles.cardText}>{followers}</p>
+        </div>
+        <div>
+            <p className={styles.cardTitle}>{title}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
