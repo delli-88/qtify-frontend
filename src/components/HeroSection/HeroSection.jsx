29e45d02f63293146e4f54dd3b5c3e85ee31a639
virinchi <rangerdelli@gmail.com>
2023-10-07 21:33:31 +0530

    hero-qtify-takehome

---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from './HeroSection.module.css'
+import HeroImage from '../../assets/HeroImage.svg' 
+
+const HeroSection = () => {
+  return (
+    <div className={styles.heroSection}>
+        <div className={styles.heroTextContainer}>
+            <p className={styles.heroText1}>100 Thousand Songs, ad-free</p>
+            <p className={styles.heroText2}>Over thousands podcast episodes</p>
+        </div>
+        <div className={styles.heroImage}>
+          <img src={HeroImage} alt="HeroImage"/>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
